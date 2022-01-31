41da6352c07df0c6401ff72244b48b83f8350bef
Derek Reid <itsjustdel@gmail.com>
2022-01-31 15:22:20 +0000

    init

---
+++ src/main/java/Bear.java
@@ -0,0 +1,39 @@
+public class Bear {
+
+    private String name;
+    private int age;
+    private double weight;
+    private char gender;
+
+    public Bear(String name, int age, double weight, char gender){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.gender = gender;
+
+    }
+    // Getter
+    public String getName(){
+        return this.name;
+    }
+
+    // Setters
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public double getWeight(){
+        return this.weight;
+    }
+
+    public boolean readyToHibernate(){
+        if (this.weight >= 80.00){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        //entry point here
+
+       // Bear bear = new Bear("Balu", 25);
+        //bear.setName("Baloo");
+        //String name = bear.getName();
+        //System.out.println(name);
+    }
+}
